0c164dd0ef138af53fc2eb9e2e68acad8a9804c7
Khizar Anjum <khizaranjum28@gmail.com>
2020-09-13 19:29:41 -0400

    fixed production mode bug

--- assets/js/search-worker-9.0.4.js
+++ assets/js/search-worker-9.0.4.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-09-13T19:12:17-04:00';
+const INDEX_KEY = 'index--2020-09-13T19:28:33-04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
