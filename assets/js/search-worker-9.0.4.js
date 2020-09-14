6c47cbebc62cbc8c307df822bd2af65fc7a7021a
Khizar Anjum <khizaranjum28@gmail.com>
2020-09-14 14:37:19 -0400

    fixed card size

--- assets/js/search-worker-9.0.4.js
+++ assets/js/search-worker-9.0.4.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2020-09-14T14:18:21-04:00';
+const INDEX_KEY = 'index--2020-09-14T14:35:48-04:00';
 const storage = new StorageArea('mini-search/');
 
 const OPTIONS = {
